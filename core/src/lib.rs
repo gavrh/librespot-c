9736f8faccea4a82de4aaa6ca0587147ce98df38
gavin <96849112+gavrh@users.noreply.github.com>
2025-02-18 04:30:05 -0800

    give rust functions rust suffix

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -6,24 +6,24 @@ pub struct Player {
 }
 
 #[no_mangle]
-pub extern "C" fn player_new() -> *mut Player {
+pub extern "C" fn player_new_rust() -> *mut Player {
     Box::into_raw(Box::new(Player { volume: 50 }))
 }
 
 #[no_mangle]
-pub extern "C" fn player_free(player: *mut Player) {
+pub extern "C" fn player_free_rust(player: *mut Player) {
     if player.is_null() { return; }
     unsafe { drop(Box::from_raw(player)) };
 }
 
 #[no_mangle]
-pub extern "C" fn player_set_volume(player: *mut Player, volume: i32) {
+pub extern "C" fn player_set_volume_rust(player: *mut Player, volume: i32) {
     if let Some(p) = unsafe { player.as_mut() } {
         p.volume = volume;
     }
 }
 
 #[no_mangle]
-pub extern "C" fn player_get_volume(player: *const Player) -> i32 {
+pub extern "C" fn player_get_volume_rust(player: *const Player) -> i32 {
     unsafe { player.as_ref().map_or(0, |p| p.volume) }
 }
