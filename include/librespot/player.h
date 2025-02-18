df90958f0f945aa0e3bc234e0e45c56943865309
gavin <96849112+gavrh@users.noreply.github.com>
2025-02-18 04:11:43 -0800

    testing change to cmake file and using extern c

--- include/librespot/player.h
+++ include/librespot/player.h
@@ -1,10 +1,18 @@
 #ifndef LIBRESPOT_PLAYER_H
 #define LIBRESPOT_PLAYER_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef struct Player Player;
 Player* player_new();
 void player_free(Player*);
 void player_set_volume(Player*, int);
 int player_get_volume(const Player*);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // LIBRESPOT_PLAYER_H
