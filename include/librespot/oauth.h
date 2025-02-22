f46976319e1bf7aadd056c594aeaf383690f37cf
gavin <96849112+gavrh@users.noreply.github.com>
2025-02-22 01:01:54 -0800

    acutally adding authenticator and oauth structs

---
+++ include/librespot/oauth.h
@@ -0,0 +1,21 @@
+#ifndef LIBRESPOT_OAUTH_H
+#define LIBRESPOT_OAUTH_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct Authenticator Authenticator;
+typedef struct OAuth OAuth;
+
+extern Authenticator* authenticator_new(const char* client_id, const char* redirect_uri);
+extern void authenticator_custom_message(Authenticator* auther, const char* message);
+extern void authenticator_open(Authenticator* auther);
+
+extern OAuth* authenticator_build(Authenticator* auther);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // LIBRESPOT_OAUTH_H
