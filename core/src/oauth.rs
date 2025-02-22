2a6378f42fdd0a07c11cee22f6ccefce1e989645
gavin <96849112+gavrh@users.noreply.github.com>
2025-02-22 04:44:41 -0800

    OAuthClientBuilder only used when building, easier to keep track of the custom message, auto open bool, and scopes

--- core/src/oauth.rs
+++ core/src/oauth.rs
@@ -1,13 +1,127 @@
 use librespot::oauth::OAuthClientBuilder;
 use librespot::oauth::OAuthClient;
 use librespot::oauth::OAuthToken;
+use std::ffi::c_int;
 use std::ffi::{CStr, CString};
 use std::ffi::c_char;
 
 #[repr(C)]
 #[derive(Debug)]
 pub struct Authenticator {
-    builder: *mut OAuthClientBuilder,
+    client_id: *const c_char,
+    redirect_uri: *const c_char,
+    message: String,
+    auto_open: bool,
+    scopes: Vec<String>,
+}
+
+#[no_mangle]
+pub fn authenticator_new(client_id: *const c_char, redirect_uri: *const c_char) -> *mut Authenticator {
+    Box::into_raw(Box::new(
+        Authenticator {
+            client_id,
+            redirect_uri,
+            message: "".to_string(),
+            auto_open: false,
+            scopes: vec![]
+        }
+    ))
+}
+
+#[no_mangle]
+pub fn authenticator_free(auther: *mut Authenticator) {
+    if auther.is_null() {
+        return;
+    }
+    unsafe {
+        drop(Box::from_raw(auther));
+    }
+}
+
+#[no_mangle]
+pub fn authenticator_custom_message(auther: *mut Authenticator, message: *const c_char) {
+    if auther.is_null() {
+        return;
+    }
+    unsafe {
+        (*auther).message = CStr::from_ptr(message).to_str().unwrap().to_string();
+    }
+}
+
+#[no_mangle]
+pub fn authenticator_auto_open(auther: *mut Authenticator) {
+    if auther.is_null() {
+        return;
+    }
+    unsafe {
+        (*auther).auto_open = true;
+    }
+}
+
+#[no_mangle]
+pub fn authenticator_add_scope(auther: *mut Authenticator, scope: c_int) {
+    if auther.is_null() {
+        return;
+    }
+    unsafe {
+        match scope {
+            1 => (*auther).scopes.push("app-remote-control".to_string()),
+            2 => (*auther).scopes.push("playlist-modify".to_string()),
+            3 => (*auther).scopes.push("playlist-modify-private".to_string()),
+            4 => (*auther).scopes.push("playlist-modify-public".to_string()),
+            5 => (*auther).scopes.push("playlist-read".to_string()),
+            6 => (*auther).scopes.push("playlist-read-collaborative".to_string()),
+            7 => (*auther).scopes.push("playlist-read-private".to_string()),
+            8 => (*auther).scopes.push("streaming".to_string()),
+            9 => (*auther).scopes.push("ugc-image-upload".to_string()),
+            10 => (*auther).scopes.push("user-follow-modify".to_string()),
+            11 => (*auther).scopes.push("user-follow-read".to_string()),
+            12 => (*auther).scopes.push("user-library-modify".to_string()),
+            13 => (*auther).scopes.push("user-library-read".to_string()),
+            14 => (*auther).scopes.push("user-modify".to_string()),
+            15 => (*auther).scopes.push("user-modify-playback-state".to_string()),
+            16 => (*auther).scopes.push("user-modify-private".to_string()),
+            17 => (*auther).scopes.push("user-personalized".to_string()),
+            18 => (*auther).scopes.push("user-read-birthdate".to_string()),
+            19 => (*auther).scopes.push("user-read-currently-playing".to_string()),
+            20 => (*auther).scopes.push("user-read-email".to_string()),
+            21 => (*auther).scopes.push("user-read-play-history".to_string()),
+            22 => (*auther).scopes.push("user-read-playback-position".to_string()),
+            23 => (*auther).scopes.push("user-read-playback-state".to_string()),
+            24 => (*auther).scopes.push("user-read-private".to_string()),
+            25 => (*auther).scopes.push("user-read-recently-played".to_string()),
+            26 => (*auther).scopes.push("user-top-read".to_string()),
+            27 => {
+                (*auther).scopes.push("app-remote-control".to_string());
+                (*auther).scopes.push("playlist-modify".to_string());
+                (*auther).scopes.push("playlist-modify-private".to_string());
+                (*auther).scopes.push("playlist-modify-public".to_string());
+                (*auther).scopes.push("playlist-read".to_string());
+                (*auther).scopes.push("playlist-read-collaborative".to_string());
+                (*auther).scopes.push("playlist-read-private".to_string());
+                (*auther).scopes.push("streaming".to_string());
+                (*auther).scopes.push("ugc-image-upload".to_string());
+                (*auther).scopes.push("user-follow-modify".to_string());
+                (*auther).scopes.push("user-follow-read".to_string());
+                (*auther).scopes.push("user-library-modify".to_string());
+                (*auther).scopes.push("user-library-read".to_string());
+                (*auther).scopes.push("user-modify".to_string());
+                (*auther).scopes.push("user-modify-playback-state".to_string());
+                (*auther).scopes.push("user-modify-private".to_string());
+                (*auther).scopes.push("user-personalized".to_string());
+                (*auther).scopes.push("user-read-birthdate".to_string());
+                (*auther).scopes.push("user-read-currently-playing".to_string());
+                (*auther).scopes.push("user-read-email".to_string());
+                (*auther).scopes.push("user-read-play-history".to_string());
+                (*auther).scopes.push("user-read-playback-position".to_string());
+                (*auther).scopes.push("user-read-playback-state".to_string());
+                (*auther).scopes.push("user-read-private".to_string());
+                (*auther).scopes.push("user-read-recently-played".to_string());
+                (*auther).scopes.push("user-top-read".to_string());
+            },
+            _ => return
+        }
+    }
 }
 
 #[repr(C)]
@@ -17,82 +131,21 @@ pub struct OAuth {
 }
 
 #[no_mangle]
-pub fn authenticator_new(client_id: *const c_char, redirect_uri: *const c_char) -> *mut Authenticator {
-    Box::into_raw(Box::new(
-        Authenticator {
-            builder: Box::into_raw(Box::new(
-                unsafe {
-                    OAuthClientBuilder::new(
-                        CStr::from_ptr(client_id).to_str().unwrap(),
-                        CStr::from_ptr(redirect_uri).to_str().unwrap(),
-                        vec![
-                            "app-remote-control",
-                            "playlist-modify",
-                            "playlist-modify-private",
-                            "playlist-modify-public",
-                            "playlist-read",
-                            "playlist-read-collaborative",
-                            "playlist-read-private",
-                            "streaming",
-                            "ugc-image-upload",
-                            "user-follow-modify",
-                            "user-follow-read",
-                            "user-library-modify",
-                            "user-library-read",
-                            "user-modify",
-                            "user-modify-playback-state",
-                            "user-modify-private",
-                            "user-personalized",
-                            "user-read-birthdate",
-                            "user-read-currently-playing",
-                            "user-read-email",
-                            "user-read-play-history",
-                            "user-read-playback-position",
-                            "user-read-playback-state",
-                            "user-read-private",
-                            "user-read-recently-played",
-                            "user-top-read"
-                        ]
-                    )
-                }
-            ))
-        }
-    ))
-}
-
-#[no_mangle]
-pub fn authenticator_custom_message(auther: *mut Authenticator, message: *const c_char) {
-    if auther.is_null() {
-        return;
-    }
-    unsafe {
-        if !(*auther).builder.is_null() {
-            let old_builder = Box::from_raw((*auther).builder);
-            let new_builder = old_builder.with_custom_message(CStr::from_ptr(message).to_str().unwrap());
-            (*auther).builder = Box::into_raw(Box::new(new_builder));
-        }
-    }
-}
-
-#[no_mangle]
-pub fn authenticator_open(auther: *mut Authenticator) {
-    if auther.is_null() {
-        return;
-    }
-    unsafe {
-        if !(*auther).builder.is_null() {
-            let old_builder = Box::from_raw((*auther).builder);
-            let new_builder = old_builder.open_in_browser();
-            (*auther).builder = Box::into_raw(Box::new(new_builder));
-        }
-    }
-}
-
-#[no_mangle]
 pub fn authenticator_build(auther: *mut Authenticator) -> *mut OAuth {
     unsafe {
-        let builder_raw = Box::from_raw((*auther).builder);
-        let oauth_client = builder_raw.build().unwrap();
+        let mut oauth_builder = OAuthClientBuilder::new(
+            CStr::from_ptr((*auther).client_id).to_str().unwrap(),
+            CStr::from_ptr((*auther).redirect_uri).to_str().unwrap(),
+            (*auther).scopes.iter().map(|s| s.as_str()).collect()
+        );
+        if !(*auther).message.is_empty() {
+            oauth_builder = oauth_builder.with_custom_message(&(*auther).message);
+        }
+        if (*auther).auto_open {
+            oauth_builder = oauth_builder.open_in_browser();
+        }
+
+        let oauth_client = oauth_builder.build().unwrap();
         let oauth_token = oauth_client.get_access_token().unwrap();
 
         authenticator_free(auther);
@@ -106,12 +159,12 @@ pub fn authenticator_build(auther: *mut Authenticator) -> *mut OAuth {
 }
 
 #[no_mangle]
-pub fn authenticator_free(auther: *mut Authenticator) {
-    if auther.is_null() {
-        return;
+pub fn oauth_free(auth: *mut OAuth) {
+    if auth.is_null() {
+        return
     }
     unsafe {
-        drop(Box::from_raw(auther));
+        drop(Box::from_raw(auth));
     }
 }
 
--- include/librespot/oauth.h
+++ include/librespot/oauth.h
@@ -6,42 +6,45 @@ extern "C" {
 #endif
 
 typedef struct Authenticator Authenticator;
-typedef enum AuthScope {
-    APP_REMOTE_CONTROL,
-    PLAYLIST_MODIFY,
-    PLAYLIST_MODIFY_PRIVATE,
-    PLAYLIST_MODIFY_PUBLIC,
-    PLAYLIST_READ,
-    PLAYLIST_READ_COLLABORATIVE,
-    PLAYLIST_READ_PRIVATE,
-    STREAMING,
-    UGC_IMAGE_UPLOAD,
-    USER_FOLLOW_MODIFY,
-    USER_FOLLOW_READ,
-    USER_LIBRARY_MODIFY,
-    USER_LIBRARY_READ,
-    USER_MODIFY,
-    USER_MODIFY_PLAYBACK_STATE,
-    USER_MODIFY_PRIVATE,
-    USER_PERSONALIZED,
-    USER_READ_BIRTHDATE,
-    USER_READ_CURRENTLY_PLAYING,
-    USER_READ_EMAIL,
-    USER_READ_PLAY_HISTORY,
-    USER_READ_PLAYBACK_POSITION,
-    USER_READ_PLAYBACK_STATE,
-    USER_READ_PRIVATE,
-    USER_READ_RECENTLY_PLAYED,
-    USER_TOP_READ
+typedef enum {
+    AUTH_SCOPE_APP_REMOTE_CONTROL = 1,
+    AUTH_SCOPE_PLAYLIST_MODIFY = 2,
+    AUTH_SCOPE_PLAYLIST_MODIFY_PRIVATE = 3,
+    AUTH_SCOPE_PLAYLIST_MODIFY_PUBLIC = 4,
+    AUTH_SCOPE_PLAYLIST_READ = 5,
+    AUTH_SCOPE_PLAYLIST_READ_COLLABORATIVE = 6,
+    AUTH_SCOPE_PLAYLIST_READ_PRIVATE = 7,
+    AUTH_SCOPE_STREAMING = 8,
+    AUTH_SCOPE_UGC_IMAGE_UPLOAD = 9,
+    AUTH_SCOPE_USER_FOLLOW_MODIFY = 10,
+    AUTH_SCOPE_USER_FOLLOW_READ = 11,
+    AUTH_SCOPE_USER_LIBRARY_MODIFY = 12,
+    AUTH_SCOPE_USER_LIBRARY_READ = 13,
+    AUTH_SCOPE_USER_MODIFY = 14,
+    AUTH_SCOPE_USER_MODIFY_PLAYBACK_STATE = 15,
+    AUTH_SCOPE_USER_MODIFY_PRIVATE = 16,
+    AUTH_SCOPE_USER_PERSONALIZED = 17,
+    AUTH_SCOPE_USER_READ_BIRTHDATE = 18,
+    AUTH_SCOPE_USER_READ_CURRENTLY_PLAYING = 19,
+    AUTH_SCOPE_USER_READ_EMAIL = 20,
+    AUTH_SCOPE_USER_READ_PLAY_HISTORY = 21,
+    AUTH_SCOPE_USER_READ_PLAYBACK_POSITION = 22,
+    AUTH_SCOPE_USER_READ_PLAYBACK_STATE = 23,
+    AUTH_SCOPE_USER_READ_PRIVATE = 24,
+    AUTH_SCOPE_USER_READ_RECENTLY_PLAYED = 25,
+    AUTH_SCOPE_USER_TOP_READ = 26,
+    AUTH_SCOPE_ALL = 27
 } AuthScope;
 
 extern Authenticator* authenticator_new(const char* client_id, const char* redirect_uri);
 extern void authenticator_free(Authenticator* auther);
 extern void authenticator_custom_message(Authenticator* auther, const char* message);
-extern void authenticator_open(Authenticator* auther);
+extern void authenticator_auto_open(Authenticator* auther);
+extern void authenticator_add_scope(Authenticator* auther, AuthScope scope);
 
 typedef struct OAuth OAuth;
 extern OAuth* authenticator_build(Authenticator* auther);
+extern void oauth_free(OAuth* auth);
 extern const char* oauth_access_token(OAuth* auth);
 extern const char* oauth_refresh_token(OAuth* auth);
 
